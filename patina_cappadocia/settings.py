bbacf6375db44c317f2b70b240fe008ffa8a5318
yusuferem <ysferem@gmail.com>
2025-01-13 14:51:56 +0300

    Update

--- patina_cappadocia/settings.py
+++ patina_cappadocia/settings.py
@@ -20,9 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-SECRET_KEY = 'django-insecure-1234567890'
-DEBUG = True
-ALLOWED_HOSTS = ['*']
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'True'  
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
